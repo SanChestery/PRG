f308d0f6370b4c403d750e93e55218d48c74a97a
SanChestery <121317406+SanChestery@users.noreply.github.com>
2023-05-23 19:28:04 +0200

    Abschluss Block06 twm aufgaben

---
+++ block06/src/ch/hslu/informatik/prg/block06/twm/twm_aufgabe5/Person.java
@@ -0,0 +1,13 @@
+package ch.hslu.informatik.prg.block06.twm.twm_aufgabe5; 
+
+public class Person {
+    private String name; 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
